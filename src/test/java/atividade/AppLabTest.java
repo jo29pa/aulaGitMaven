c5465dc34216bd429f2d328ccd3a07003f086eec
ADM <ADM@c-lab01-pc14>
2018-03-12 20:51:32 -0300

    1º comit

---
+++ src/test/java/atividade/AppLabTest.java
@@ -0,0 +1,23 @@
+
+package atividade;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppLabTest {
+    
+    public AppLabTest() {
+    }
+
+    /**
+    * Test oflerJSONmethod, ofclassAppLab.
+    */
+    @Test
+    public void testLerJSON() throws Exception{
+        System.out.println("lerJSON");
+        String url= "http://time.jsontest.com/"; // esta URL gera um conteúdo JSON
+        String result= AppLab.lerJSON(url);
+        assertTrue(result.length() > 0);
+    }
+    
+}
